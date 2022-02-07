d0b45b99f723ce488c0dea459e65debe3d45f175
v.manousakis <v.manousakis@pan-net.eu>
2022-02-07 13:31:58 +0200

    process_data.py: addedd if_exists='replace' parameter in the  - Improved Doctrings in the functions -  Added type hinting

--- data/process_data.py
+++ data/process_data.py
@@ -13,7 +13,7 @@ import re
 import sys
 import os
 
-def load_data(messages_filepath, categories_filepath):
+def load_data(messages_filepath : str, categories_filepath : str) -> pd.DataFrame:
     """Load messages.csv & categories.csv and return a merged dataframe.
 
     Args:
@@ -24,17 +24,17 @@ def load_data(messages_filepath, categories_filepath):
         DataFrame: Messages & categories merged dataframe.
     """
 
-    messages_df = pd.read_csv(messages_filepath)
-    categories_df = pd.read_csv(categories_filepath)
+    messages_df     = pd.read_csv(messages_filepath)
+    categories_df   = pd.read_csv(categories_filepath)
 
     return pd.merge(messages_df, categories_df, on="id")
 
 
-def clean_data(df):
-    """[summary]
+def clean_data(df : pd.DataFrame) -> pd.DataFrame:
+    """ Remove links, punctuations & unknown words.
 
     Args:
-        df (DataFrame): DataFrame containing the text messages and the corresponding target labels..
+        df (DataFrame): DataFrame containing the text messages and the corresponding target labels.
 
     Returns:
         df (DataFrame): Cleaned dataframe.
@@ -62,28 +62,30 @@ def clean_data(df):
     df['message'] = df['message'].str.replace('[^\w\s]','')
     df['message'] = df['message'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
     df.drop(columns=['related', 'child_alone'], axis=1, inplace=True)
+
     return df
 
 
-def save_data(df, database_filename):
-    """
+def save_data(df : pd.DataFrame, database_filename : str):
+    """Saves the cleaned DataFrame as sql database instance locally.
 
     Args:
-        df ([type]): [description]
-        database_filename ([type]): [description]
+        df (DataFrame): Cleaned DataFrame.
+        database_filename (String): The name of the database
+    
+    Returns:
+        None.
     """
 
-    """
-    Saves the data into a database file.
-    :param df:
-    :param database_filename:
-    :return:
-    """
-
+    # Initialize the sql lite engine.
     engine = create_engine(f'sqlite:///{database_filename}')
+
+    # Create the table name.
     table_name = os.path.basename(
         database_filename).replace(".db", "") + "_table"
-    df.to_sql(table_name, engine, index=False)
+
+    # Save the sql table locally. The path the DB is saved, is the same as the path the process_data.py resides in.  
+    df.to_sql(table_name, engine, index=False, if_exists='replace')
     
 
 def main():
